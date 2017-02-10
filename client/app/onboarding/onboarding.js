bd273dc9d29bf90236784e41d27ae4b80dc8bf0d
Elizabeth Akerman <ekakerman@gmail.com>
2017-02-09 16:43:59 -0800

    add dummy data

--- client/app/onboarding/onboarding.js
+++ client/app/onboarding/onboarding.js
@@ -1,4 +1,21 @@
 angular.module('app.onboarding', [])
   .controller('onboardingController', function($scope) {
     $scope.value = '(onboarding instructions here)';
+    $scope.sites = [
+      {
+        id: 1,
+        url: 'www.amazon.com',
+        title: 'My Amazon Marketplace'
+      },
+      {
+        id: 2,
+        url: 'www.blog.com',
+        title: 'My Blog'
+      },
+      {
+        id: 3,
+        url: 'www.business.com',
+        title: 'My Business\'s Website'
+      }
+    ];
   });
