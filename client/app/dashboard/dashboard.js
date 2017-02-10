ac29340a1f2735b662645b8b90001ef261e8bc4e
Elizabeth Akerman <ekakerman@gmail.com>
2017-02-09 16:51:33 -0800

    add/render sitelist

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -1,4 +1,21 @@
 angular.module('app.dashboard', [])
   .controller('dashboardController', function($scope) {
     $scope.value = '(website list here)';
+    $scope.sites = [
+      {
+        id: 1,
+        url: 'www.amazon.com',
+        title: 'My Amazon Marketplace'
+      },
+      {
+        id: 2,
+        url: 'www.blog.com',
+        title: 'My Blog'
+      },
+      {
+        id: 3,
+        url: 'www.business.com',
+        title: 'My Business\'s Website'
+      }
+    ];
   });
