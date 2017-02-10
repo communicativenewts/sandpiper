9e0f8147558faede93839e072749a7e190a11a97
Elizabeth Akerman <ekakerman@gmail.com>
2017-02-09 17:10:21 -0800

    format dashboard

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -1,21 +1,27 @@
 angular.module('app.dashboard', [])
   .controller('dashboardController', function($scope) {
-    $scope.value = '(website list here)';
+
     $scope.sites = [
       {
         id: 1,
         url: 'www.amazon.com',
-        title: 'My Amazon Marketplace'
+        title: 'My Amazon Marketplace',
+        status: 'active',
+        date: 'January 4, 2017'
       },
       {
         id: 2,
         url: 'www.blog.com',
-        title: 'My Blog'
+        title: 'My Blog',
+        status: 'suspended',
+        date: 'May 23, 2016'
       },
       {
         id: 3,
         url: 'www.business.com',
-        title: 'My Business\'s Website'
+        title: 'My Business\'s Website',
+        status: 'active',
+        date: 'September 6, 2016'
       }
     ];
   });
