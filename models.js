0c092c5a16c0845df62f20675f380a1150309a6a
Alex Autem <autem.alex@gmail.com>
2017-02-09 19:24:52 -0500

    Add new models to database

--- models.js
+++ models.js
@@ -2,8 +2,27 @@
 var mongoose = require('mongoose');
 var db = require('./database.js')
 
+//create new users schema
+var userSchema = mongoose.Schema({
+  _id: Number,
+  username: String,
+  password: String,
+  email: String,
+  sites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Site' }]
+});
+
+var siteSchema = mongoose.Schema({
+  _id: Number,
+  _creator: { type: Number, ref: 'User' },
+  url: String,
+  title: String,
+  clicks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'linkClickSchema' }],
+  views: [{ type: mongoose.Schema.Types.ObjectId, ref: 'pageViewSchema' }]
+});
+
 //create new linkClick schema
 var linkClickSchema = mongoose.Schema({
+  _site: { type: Number, ref: 'Site' },
   url: String,
   count: Number,
   date: Array
@@ -11,17 +30,22 @@ var linkClickSchema = mongoose.Schema({
 
 //create new pageView schema
 var pageViewSchema = mongoose.Schema({
+  _site: { type: Number, ref: 'Site' },
   title: String,
   count: Number,
   date: Array
 });
 
 //create models for each schema
+var User = mongoose.model('User', userSchema);
+var Site = mongoose.model('Site', siteSchema);
 var linkClickModel = mongoose.model('linkClickSchema', linkClickSchema);
 var pageViewModel = mongoose.model('pageViewSchema', pageViewSchema);
 
 //export models
 module.exports = {
+  User: User,
+  Site: Site,
   linkClickModel: linkClickModel,
-  pageViewModel: pageViewModel,
+  pageViewModel: pageViewModel
 };
