0f37a45abba0719f2e2fc3dfff3aafec3c14ea67
Elizabeth Akerman <ekakerman@gmail.com>
2017-02-11 14:28:58 -0800

    add data validation

--- client/app/onboarding/onboarding.js
+++ client/app/onboarding/onboarding.js
@@ -1,6 +1,23 @@
 angular.module('app.onboarding', [])
   .controller('onboardingController', function($scope) {
 
+    $scope.newSite;
+    $scope.validURL = '';
+
+    $scope.checkURL = function() {
+      console.log('checking Url');
+      var regEx = /^[-a-zA-Z0-9@:%._\+~#=]{2,256}\.([-a-zA-Z0-9@:%._\+~#=]{2,256}\.)*[a-z]{2,6}$/;
+      if (regEx.test($scope.newSite)) {
+        console.log('valid site');
+        $scope.validURL = 'Valid Site';
+        $('#site-input').addClass("has-success");
+      } else {
+        console.log('invalid site');
+        $scope.validURL = 'Invalid Site';
+        $('#site-input').addClass("has-error");
+      }
+    };
+
     $scope.script = 'Please complete Step 1 to access your custom script.';
 
     $scope.createScript = function() {
