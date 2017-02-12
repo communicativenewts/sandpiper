1a6599fe492d419b079abd3bfcb1b538ea7b5b35
Elizabeth Akerman <ekakerman@gmail.com>
2017-02-11 19:46:03 -0800

    add onboarding functionality

--- client/app/onboarding/onboarding.js
+++ client/app/onboarding/onboarding.js
@@ -1,5 +1,5 @@
 angular.module('app.onboarding', [])
-  .controller('onboardingController', function($scope, Users) {
+  .controller('onboardingController', function($scope, Users, $location) {
 
     $scope.userId = Users.getUserId();
     $scope.newSite = {
@@ -7,37 +7,53 @@ angular.module('app.onboarding', [])
       title: ''
     };
 
-    $scope.validURL = '';
-    $scope.validTitle = '';
+    $scope.validURL = null;
+    $scope.validTitle = null;
     $scope.siteId = null;
 
-    $scope.checkURL = function() {
-      console.log('checking Url');
+    $scope.checkSite = function() {
+
+      //check for valid title
+      if ($scope.newSite.title.length > 0) {
+        console.log('valid title');
+        $scope.validTitle = 'Valid Title';
+        $('#title-input').removeClass('has-error').addClass('has-success');
+      } else {
+        console.log('invalid title');
+        $scope.validTitle = 'Please Add Title';
+        $('#title-input').removeClass('has-success').addClass('has-error');
+      };
+
+      $('#title-valid').show();
+
       var regEx = /^[-a-zA-Z0-9@:%._\+~#=]{2,256}\.([-a-zA-Z0-9@:%._\+~#=]{2,256}\.)*[a-z]{2,6}$/;
-      if (regEx.test($scope.newSite)) {
+
+      if (regEx.test($scope.newSite.url)) {
         console.log('valid site');
-        $scope.validURL = 'Valid Site';
-        $('#site-input').removeClass('has-error');
-        $('#site-input').addClass('has-success');
+        $scope.validURL = 'Valid Site URL';
+        $('#site-input').removeClass('has-error').addClass('has-success');
       } else {
         console.log('invalid site');
-        $scope.validURL = 'Invalid Site';
-        $('#site-input').removeClass('has-success');
-        $('#site-input').addClass('has-error');
+        $scope.validURL = 'Invalid Site URL';
+        $('#site-input').removeClass('has-success').addClass('has-error');
+      }
+
+      $('#url-valid').show();
+
+      if ($scope.validTitle === 'Valid Title' && $scope.validURL === 'Valid Site URL'){
+        createScript();
       }
     };
 
     $scope.script = 'Please complete Step 1 to access your custom script.';
 
-    $scope.createScript = function() {
-      if ($scope.newSite.title.length === 0) {
-        $scope.validTitle = 'Please give this website a title';
-        return;
-      }
-      Users.addNewSite($scope.userId, $scope.newSite)
+    var createScript = function() {
+      console.log('createScript triggered');
+      Users.addNewSite(Users.getUserId(), $scope.newSite)
       .then(function(siteId) {
-        $scope.siteId = siteId;
-        $scope.script = '<script>window.sandpiperid = \"' + siteId + '\"</script>\n<script src = "scriptsource.com"></script>';
+        console.log('new site id is', siteId)
+        $scope.siteId = siteId._id;
+        $scope.script = '<script>window.sandpiperid=\"' + $scope.siteId + '\"</script>\n<script src="scriptsource.com"></script>';
       });
     };
 
