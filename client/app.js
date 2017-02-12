3739a04641ab462c0ec95a66b918b598324f67eb
Alex Autem <autem.alex@gmail.com>
2017-02-12 00:02:45 -0500

    Add authentication parameter to routes requiring login

--- client/app.js
+++ client/app.js
@@ -22,29 +22,57 @@ angular.module('sharkanalytics',
     })
     .when('/onboarding', {
       templateUrl: 'app/onboarding/onboarding.html',
-      controller: 'onboardingController'
+      controller: 'onboardingController',
+      authenticate: true
     })
     .when('/dashboard', {
       templateUrl: 'app/dashboard/dashboard.html',
-      controller: 'dashboardController'
+      controller: 'dashboardController',
+      authenticate: true
     })
     .when('/overallStats', {
       templateUrl: 'app/overall/overall.html',
-      controller: 'linkClickController'
+      controller: 'linkClickController',
+      authenticate: true
     })
     .when('/pageView', {
       templateUrl: 'app/pageview/pageView.html',
-      controller: 'pageViewController'
+      controller: 'pageViewController',
+      authenticate: true
     })
     .when('/linkClick', {
       templateUrl: 'app/linkclicks/linkClick.html',
-      controller: 'linkClickController'
-    })
-    .when('/overall', {
-      templateUrl: 'app/overall/overall.html',
-      controller: 'linkClickVisualsController'
+      controller: 'linkClickController',
+      authenticate: true
     })
     .otherwise({
       redirectTo: '/login'
+<<<<<<< HEAD
     })
   });
+=======
+    });
+
+  $httpProvider.interceptors.push('AttachTokens');
+})
+.factory('AttachTokens', function($window) {
+  var attach = {
+    request: function(object) {
+      var jwt = $window.localStorage.getItem('sandpiper.analytics'); // Auth.js
+      if (jwt) {
+        object.headers['x-access-token'] = jwt;
+      }
+      object.headers['Allow-Control-Allow-Origin'] = '*';
+      return object;
+    }
+  };
+  return attach;
+})
+.run(function($rootScope, $location, Auth) {
+  $rootScope.$on('$routeChangeStart', function(evt, next, current) {
+    if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+      $location.path('/login');
+    }
+  });
+});
+>>>>>>> Add authentication parameter to routes requiring login
