ad3c61dc6e1bc02e45e7202deab73c77dff61156
Alex Autem <autem.alex@gmail.com>
2017-02-13 19:30:04 -0500

    Remove console.logs in onboarding

--- client/app/onboarding/onboarding.js
+++ client/app/onboarding/onboarding.js
@@ -15,11 +15,9 @@ angular.module('app.onboarding', [])
 
       //check for valid title
       if ($scope.newSite.title.length > 0) {
-        console.log('valid title');
         $scope.validTitle = 'Valid Title';
         $('#title-input').removeClass('has-error').addClass('has-success');
       } else {
-        console.log('invalid title');
         $scope.validTitle = 'Please Add Title';
         $('#title-input').removeClass('has-success').addClass('has-error');
       };
@@ -29,11 +27,9 @@ angular.module('app.onboarding', [])
       var regEx = /^[-a-zA-Z0-9@:%._\+~#=]{2,256}\.([-a-zA-Z0-9@:%._\+~#=]{2,256}\.)*[a-z]{2,6}$/;
 
       if (regEx.test($scope.newSite.url)) {
-        console.log('valid site');
         $scope.validURL = 'Valid Site URL';
         $('#site-input').removeClass('has-error').addClass('has-success');
       } else {
-        console.log('invalid site');
         $scope.validURL = 'Invalid Site URL';
         $('#site-input').removeClass('has-success').addClass('has-error');
       }
@@ -48,10 +44,8 @@ angular.module('app.onboarding', [])
     $scope.script = 'Please complete Step 1 to access your custom script.';
 
     var createScript = function() {
-      console.log('createScript triggered');
       Users.addNewSite(Users.getUserId(), $scope.newSite)
       .then(function(siteId) {
-        console.log('new site id is', siteId)
         $scope.siteId = siteId._id;
         $scope.script = '<script>window.sandpiperid=\"' + $scope.siteId + '\"</script>\n<script src="https://cdn.rawgit.com/communicativenewts/sandpiper/aba4852667e4011ed6c24f850633b41ec20bc664/tracking/sandpiper_analytics.js"></script>';
       });
